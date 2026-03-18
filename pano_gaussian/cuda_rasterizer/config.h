e3a7b7de4d4bb39c378b260ef2170cf36e43c3ee
eacsai <1349157283@qq.com>
2026-03-18 23:58:41 +0800

    feat: add pano-gaussian and add single iamge process for pixel_gs.py

---
+++ pano_gaussian/cuda_rasterizer/config.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2023, Inria
+ * GRAPHDECO research group, https://team.inria.fr/graphdeco
+ * All rights reserved.
+ *
+ * This software is free for non-commercial, research and evaluation use 
+ * under the terms of the LICENSE.md file.
+ *
+ * For inquiries contact  george.drettakis@inria.fr
+ */
+
+#ifndef CUDA_RASTERIZER_CONFIG_H_INCLUDED
+#define CUDA_RASTERIZER_CONFIG_H_INCLUDED
+
+#define NUM_COLOR_CHANNELS 3 // Default 3, RGB
+#define NUM_FEATURE_CHANNELS 32 // 64
+#define CAMERA_TYPE 1 // 0: perspective, 1: panorama
+
+#define BLOCK_X 16
+#define BLOCK_Y 16
+
+#endif
